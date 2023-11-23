e42ba1532dcc8ef4272079364ccba80782d4cf9a
reshmana <reshmanaik.xworkz@gmail.com>
2023-11-23 10:15:06 +0530

    1st database

---
+++ new sep_27.sql
@@ -0,0 +1,8 @@
+/*syntax of database*/
+CREATE DATABASE sep_27;
+
+/*syntax for use*/
+USE sep_27;
+
+/*syntax for show database*/
+SHOW databases;
---
+++ sep_27.sql
@@ -0,0 +1,4 @@
+/*sytaxs for database*/
+CREATE DATABASE  sep_27;
+USE sep_27
+SHOW databases;
